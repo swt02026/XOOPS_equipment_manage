2b143bcd0292e396788b821196a9cd1b82c2e015
Unknown <swt02026@gmail.com>
2016-10-27 14:27:09 +0800

    add about.php

---
+++ admin/about.php
@@ -0,0 +1,6 @@
+<?php
+    include '../../../include/cp_header.php';
+    xoops_cp_header();
+    $xoopsTpl->display('db:equipment_about.html');
+    xoops_cp_footer();
+?>
---
+++ admin/manage.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjes
+ * Date: 2016/10/27
+ * Time: 下午 02:26
+ */
+?>
