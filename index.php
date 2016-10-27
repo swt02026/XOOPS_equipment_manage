9cc3cd5e759b3dce88a9eb658a08ad5dbd1f5de5
Unknown <swt02026@gmail.com>
2016-10-27 14:18:43 +0800

    fix bug

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     include '../../mainfile.php';
-    include XOOPS_ROOT_PATH.".header.php";
+    include XOOPS_ROOT_PATH."/header.php";
 
-    include_once XOOPS_ROOT_PATH.".footer.php";
+    include_once XOOPS_ROOT_PATH."/footer.php";
 ?>
