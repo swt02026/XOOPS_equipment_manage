dfab8d829d7801f7ff1c157abc73830e96db2ce0
Unknown <swt02026@gmail.com>
2016-10-31 14:01:47 +0800

    test

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -2,13 +2,17 @@ CREATE TABLE `equipment_owner` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `owner` VARCHAR (10) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+)
+  COLLATE='utf8_general_ci'
+  ENGINE=InnoDB;
 CREATE TABLE `equipment_desc` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR (30) NOT NULL DEFAULT '',
   `image` LONGBLOB NOT NULL ,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`id`) REFERENCES equipment_owner(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB;
+)
+  COLLATE='utf8_general_ci'
+  ENGINE=InnoDB;
 
 
