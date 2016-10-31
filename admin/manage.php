b338cee758f33c8f610042b21c9cbe9f136eab8f
Unknown <swt02026@gmail.com>
2016-10-31 13:40:47 +0800

    test

--- admin/manage.php
+++ admin/manage.php
@@ -3,10 +3,10 @@
     include '../../../include/cp_header.php';
     xoops_cp_header();
 
-    $sql = sprintf("SELECT %1$s.name
-                , %2$s.owner 
-                FROM %1$s 
-                INNER JOIN %2$s 
+    $sql = sprintf("SELECT %1$s.name \
+                , %2$s.owner \
+                FROM %1$s  \
+                INNER JOIN %2$s \ 
                  ON %1$s.id=%2$s.id",
         $xoopsDB->prefix('equipment_desc'), $xoopsDB('equipment_owner'));
 
