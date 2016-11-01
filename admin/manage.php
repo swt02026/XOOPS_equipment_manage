fe995321f1c7e89ab1653df6b3219c47d288031c
Unknown <swt02026@gmail.com>
2016-11-01 10:27:11 +0800

    test

--- admin/manage.php
+++ admin/manage.php
@@ -3,12 +3,8 @@
     include '../../../include/cp_header.php';
     xoops_cp_header();
 
-    $sql = sprintf("SELECT %1$s.name
-                , %2$s.owner 
-                FROM %1$s 
-                INNER JOIN %2$s 
-                 ON %1$s.id=%2$s.id",
-        $xoopsDB->prefix('equipment_desc'), $xoopsDB->prefix('equipment_owner'));
+    $sql = sprintf("SELECT owner, name",
+        $xoopsDB->prefix('equipment_desc'));
 
     $query = $xoopsDB->query($sql);
     var_dump($query);
--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,17 +1,9 @@
-CREATE TABLE `equipment_owner` (
-  `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `owner` VARCHAR (10) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-)
-  COLLATE='utf8_general_ci'
-  ENGINE=InnoDB;
 CREATE TABLE `equipment_desc` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `owner` VARCHAR (10) NOT NULL DEFAULT '',
   `name` VARCHAR (30) NOT NULL DEFAULT '',
   `image` LONGBLOB NOT NULL ,
-  CONSTRAINT test FOREIGN KEY (`id`) REFERENCES equipment_owner(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 )
   COLLATE='utf8_general_ci'
   ENGINE=InnoDB;
