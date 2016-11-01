9e9ef53fa68c717b0eec2e52000c8c6a42baa06c
Unknown <swt02026@gmail.com>
2016-11-01 08:36:16 +0800

    test

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -6,10 +6,12 @@ CREATE TABLE `equipment_owner` (
   COLLATE='utf8_general_ci'
   ENGINE=InnoDB;
 CREATE TABLE `equipment_desc` (
-  `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR (30) NOT NULL DEFAULT '',
   `image` LONGBLOB NOT NULL ,
-  PRIMARY KEY (`id`)
+  CONSTRAINT test FOREIGN KEY (`id`) REFERENCES equipment_owner(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 )
   COLLATE='utf8_general_ci'
   ENGINE=InnoDB;
