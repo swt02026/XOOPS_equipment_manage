4c2e99857a9d2b938bdc596e213596052671e5a6
Unknown <swt02026@gmail.com>
2016-11-02 10:30:15 +0800

    fix bug

--- admin/crud/append.php
+++ admin/crud/append.php
@@ -17,7 +17,7 @@
         $post_data_amount = intval($post_data["amount"]);
         $owner = $xoopsUser->uname();
 
-        $sql = sprintf("INSERT INTO %s(`name`, `owner`, `amount`) VALUES(NULL, '{$post_data_name}', '{$owner}', {$post_data_amount});"
+        $sql = sprintf("INSERT INTO %s(`name`, `owner`, `amount`) VALUES('{$post_data_name}', '{$owner}', {$post_data_amount});"
             , $xoopsDB->prefix('equipment_desc'));
 
         //echo $sql;
@@ -25,5 +25,5 @@
     }
 
 
-   // echo "<script>window.location.href='../manage.php';</script>";
+    echo "<script>window.location.href='../manage.php';</script>";
 
