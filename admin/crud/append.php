95b92830ab9561c780026382c5c72eabce02582a
Unknown <swt02026@gmail.com>
2016-11-02 10:10:00 +0800

    test

--- admin/crud/append.php
+++ admin/crud/append.php
@@ -17,7 +17,7 @@
         $post_data_amount = intval($post_data["amount"]);
         $owner = $xoopsUser->uname();
 
-        $sql = sprintf("BEGIN; INSERT INTO %s VALUES(NULL, '{$post_data_name}', '{$owner}', {$post_data_amount}); COMMIT"
+        $sql = sprintf("INSERT INTO %s VALUES(NULL, '{$post_data_name}', '{$owner}', {$post_data_amount});"
             , $xoopsDB->prefix('equipment_manage'));
 
         echo $sql;
