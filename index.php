dfc47ae42539cb1d1b61b4f564861bf1ea014543
Unknown <swt02026@gmail.com>
2016-11-07 10:03:46 +0800

    test

--- index.php
+++ index.php
@@ -3,5 +3,20 @@
     $xoopsOption['template_main'] = "equipment_borrow.html";
     include XOOPS_ROOT_PATH."/header.php";
 
+    $sql = sprintf("SELECT  name, owner, amount, id  FROM %s",
+        $xoopsDB->prefix('equipment_desc'));
+
+    $query = $xoopsDB->query($sql);
+
+    if($xoopsDB->getRowsNum($query) > 0){
+
+        $query_rows = [];
+        while ($row = $xoopsDB->fetchArray($query)){
+
+            $query_rows[] = $row;
+        }
+        $xoopsTpl->assign('query_rows', $query_rows);
+    }
+
     include_once XOOPS_ROOT_PATH."/footer.php";
 ?>
