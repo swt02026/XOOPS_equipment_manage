31b8ff475134d83e9f38ad629798d1efa34a6ee8
Unknown <swt02026@gmail.com>
2016-11-15 10:31:36 +0800

    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     include '../../mainfile.php';
-    $xoopsOption['template_main'] = "equipment_borrow.html";
+    $xoopsOption['template_main'] = "equipment_borrow2.html";
     include XOOPS_ROOT_PATH."/header.php";
 
     $sql = sprintf("SELECT name, owner, amount, id  FROM %s",
@@ -15,7 +15,7 @@
 
             $query_rows[] = $row;
         }
-        $xoopsTpl->assign('query_rows', $query_rows);
+        //$xoopsTpl->assign('query_rows', $query_rows);
     }
 
     include_once XOOPS_ROOT_PATH."/footer.php";
