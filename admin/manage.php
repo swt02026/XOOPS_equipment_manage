8f5db08864cd151d8136bb5791053b74d5ec6d68
Unknown <swt02026@gmail.com>
2016-11-17 15:36:39 +0800

    test

--- admin/manage.php
+++ admin/manage.php
@@ -8,17 +8,16 @@
 
     $query = $xoopsDB->query($sql);
 
-
+    $query_rows = [];
     if($xoopsDB->getRowsNum($query) > 0){
 
-        $query_rows = [];
         while ($row = $xoopsDB->fetchArray($query)){
             $query_rows[] = $row;
         }
-        $json_data = json_encode($query_rows);
-        $xoopsTpl->assign('json_data', $json_data);
     }
-
+    
+    $json_data = json_encode($query_rows);
+    $xoopsTpl->assign('json_data', $json_data);
     $xoopsTpl->display('db:equipment_manage.html');
     xoops_cp_footer();
 ?>
