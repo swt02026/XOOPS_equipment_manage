5d2fdea555072dc7a63eeac29bea49695fde02e2
Unknown <swt02026@gmail.com>
2016-11-21 14:23:40 +0800

    test

--- admin/crud/update.php
+++ admin/crud/update.php
@@ -14,14 +14,20 @@
         strlen( $update_data["name"]) &&
         strlen($update_data["amount"]) &&
         intval($update_data["amount"]) > 0 &&
-        intval($update_data["amount"]) > 0
+        intval($update_data["amount"]) > 0 &&
+        intval($update_data["amount_diff"])
     ){
 
         $update_name = $update_data["name"];
         $update_amount = $update_data["amount"];
         $update_id = $update_data['update_id'];
+        $amount_diff = intval($update_data["amount_diff"]);
         $owner = $xoopsUser->uname();
-        $sql = sprintf("UPDATE `%s` SET `name`='{$update_name}', `amount`={$update_amount} WHERE `id`={$update_id} and `owner`='{$owner}'" ,
+        $sql = sprintf("UPDATE `%s` 
+            SET `name`='{$update_name}', 
+            `totalAmount`={$update_amount},
+            `amount`=`amount` + {$amount_diff}
+            WHERE `id`={$update_id} and `owner`='{$owner}'" ,
             $xoopsDB->prefix('equipment_desc')
         );
         $xoopsDB->queryF($sql);
