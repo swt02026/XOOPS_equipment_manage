c3df7138be8eba06197a94305fd9b908d8ab7485
Unknown <swt02026@gmail.com>
2016-11-28 16:13:18 +0800

    check login

--- admin/crud/borrow.php
+++ admin/crud/borrow.php
@@ -6,33 +6,34 @@
  * Time: 下午 02:40
  */
     include '../../../../mainfile.php';
-    $borrow_data = array_filter(
-        array_map("intval", $_POST["borrow_number"]), function ($val){
-        return $val > 0;
-    });
+    if($xoopsUser){
+        $borrow_data = array_filter(
+            array_map("intval", $_POST["borrow_number"]), function ($val){
+            return $val > 0;
+        });
 
-    if(sizeof($borrow_data) > 0){
+        if(sizeof($borrow_data) > 0){
 
-        $borrower = $xoopsUser->uname();
-        foreach ($borrow_data as $id => $amount){
+            $borrower = $xoopsUser->uname();
+            foreach ($borrow_data as $id => $amount){
 
-            $id = intval($id);
-            $amount = intval($amount);
-            $sql_amount_dec = sprintf("UPDATE `%s` 
-                                        SET `amount`=`amount`-{$amount} 
-                                        WHERE `id`={$id} and `amount`>={$amount}",
-                                    $xoopsDB->prefix('equipment_desc'));
+                $id = intval($id);
+                $amount = intval($amount);
+                $sql_amount_dec = sprintf("UPDATE `%s` 
+                                            SET `amount`=`amount`-{$amount} 
+                                            WHERE `id`={$id} and `amount`>={$amount}",
+                                        $xoopsDB->prefix('equipment_desc'));
 
-            $xoopsDB->queryF($sql_amount_dec);
+                $xoopsDB->queryF($sql_amount_dec);
 
-            $sql = sprintf("INSERT INTO `%s` (`id`, `amount`, `borrower`) 
-                            VALUES ({$id}, {$amount}, '{$borrower}') 
-                            ON DUPLICATE KEY UPDATE `amount`=`amount`+{$amount}",
-                        $xoopsDB->prefix('equipment_borrow'));
+                $sql = sprintf("INSERT INTO `%s` (`id`, `amount`, `borrower`) 
+                                VALUES ({$id}, {$amount}, '{$borrower}') 
+                                ON DUPLICATE KEY UPDATE `amount`=`amount`+{$amount}",
+                            $xoopsDB->prefix('equipment_borrow'));
 
-            $xoopsDB->queryF($sql);
+                $xoopsDB->queryF($sql);
 
+            }
         }
     }
-
     header("location: ../../index.php");
