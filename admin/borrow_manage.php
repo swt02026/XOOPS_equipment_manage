16cf5eeafcd1585f9538ed8e94b5e32649ff16e3
Unknown <swt02026@gmail.com>
2016-12-07 16:34:03 +0800

    fix bug

--- admin/borrow_manage.php
+++ admin/borrow_manage.php
@@ -5,7 +5,7 @@
 
     $owner = $xoopsUser->uname();
 
-    $sql = sprintf('SELECT `borrower`, `%1$s`.`amount`, `%1$s`.`id`, `name`, `owner`,  
+    $sql = sprintf('SELECT `borrower`, `%1$s`.`amount`, `%1$s`.`id`, `name`, `owner`  
                     FROM `%1$s` 
                     INNER JOIN `%2$s` ON `%1$s`.`id`=`%2$s`.`id`',
         $xoopsDB->prefix('equipment_borrow'), $xoopsDB->prefix('equipment_desc'));
