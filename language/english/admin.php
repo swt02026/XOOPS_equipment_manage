ba4361a7bb1deb72ad46f1fc41644d3f25b7fd93
mambax7 <mambax7@gmail.com>
2017-07-07 19:07:44 -0400

    added docs & assets folders

---
+++ language/english/admin.php
@@ -0,0 +1,10 @@
+<?php
+
+define('_AM_XXXXX_UPGRADEFAILED0', "Update failed - couldn't rename field '%s'");
+define('_AM_XXXXX_UPGRADEFAILED1', "Update failed - couldn't add new fields");
+define('_AM_XXXXX_UPGRADEFAILED2', "Update failed - couldn't rename table '%s'");
+define('_AM_XXXXX_ERROR_COLUMN', 'Could not create column in database : %s');
+define('_AM_XXXXX_ERROR_BAD_XOOPS', 'This module requires XOOPS %s+ (%s installed)');
+define('_AM_XXXXX_ERROR_BAD_PHP', 'This module requires PHP version %s+ (%s installed)');
+define('_AM_XXXXX_ERROR_TAG_REMOVAL', 'Could not remove tags from Tag Module');
+
---
+++ language/english/modinfo.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @copyright    XOOPS Project http://xoops.org/
+ * @license      GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package
+ * @since
+ * @author     XOOPS Development Team
+ */
+
+// The name of this module
+define('_MI_XXXXX_NAME', 'YYYYY Module Name');
+
+//Help
+define('_MI_XXXXX_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_XXXXX_HELP_HEADER', __DIR__.'/help/helpheader.html');
+define('_MI_XXXXX_BACK_2_ADMIN', 'Back to Administration of ');
+define('_MI_XXXXX_OVERVIEW', 'Overview');
+
+//define('_MI_XXXXX_HELP_DIR', __DIR__);
+
+//help multi-page
+define('_MI_XXXXX_DISCLAIMER', 'Disclaimer');
+define('_MI_XXXXX_LICENSE', 'License');
+define('_MI_XXXXX_SUPPORT', 'Support');
+//define('_MI_XXXXX_REQUIREMENTS', 'Requirements');
+//define('_MI_XXXXX_CREDITS', 'Credits');
+//define('_MI_XXXXX_HOWTO', 'How To');
+//define('_MI_XXXXX_UPDATE', 'Update');
+//define('_MI_XXXXX_INSTALL', 'Install');
+//define('_MI_XXXXX_HISTORY', 'History');
+//define('_MI_XXXXX_HELP1', 'YYYYY');
+//define('_MI_XXXXX_HELP2', 'YYYYY');
+//define('_MI_XXXXX_HELP3', 'YYYYY');
+//define('_MI_XXXXX_HELP4', 'YYYYY');
+//define('_MI_XXXXX_HELP5', 'YYYYY');
+//define('_MI_XXXXX_HELP6', 'YYYYY');
